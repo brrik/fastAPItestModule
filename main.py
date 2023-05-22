1e65cd1543a2a3a6d6d7af508735faa85f254015
brrik <60531730+brrik@users.noreply.github.com>
2023-05-22 11:13:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ async def show_result():
     global freeRetData
     global retData
     sumRet = 0
-    for i in range(len(retData)):
-        sumRet = int(sumRet) + int(retData[i])
+    for i in counts:
+        sumRet = sumRet + i
     
     if sumRet == 0:
         return freeRetData
